c3f39eab53f8a44b6dec84c62421e0786689c320
Mavis <litten@chesshub.fun>
2026-07-14 17:15:09 +0800

    delete the gobang

--- gobang-web/src/games/gobang/index.ts
+++
@@ -1,7 +0,0 @@
-export { GobangGame } from './components/GobangGame';
-export { GobangGame as default } from './components/GobangGame';
-export type { GobangState } from './types';
-export { BOARD_SIZE, WIN_COUNT } from './types';
-export { createGobangReducer } from './hooks/useGobangReducer';
-export { findBestMove, AI_DEPTHS } from './ai';
-export { INITIAL_GOBANG_STATE } from './types';
--- gobang-web/src/hooks/useGameReducer.ts
+++
@@ -1,4 +0,0 @@
-export { createGobangReducer } from '../games/gobang/hooks/useGobangReducer';
-export { INITIAL_GOBANG_STATE } from '../games/gobang/types';
-export type { GobangState } from '../games/gobang/types';
-export { findBestMove, AI_DEPTHS } from '../games/gobang/ai';
